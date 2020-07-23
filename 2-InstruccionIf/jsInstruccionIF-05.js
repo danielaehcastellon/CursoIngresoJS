ae626eeba36a9a7ba3a4b7ba21248a01abc4da21
danielaehcastellon <danielaehcastellon@gmail.com>
2020-07-22 22:41:48 -0300

    tp 3 entrada salida

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,14 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let vEdad;
+	vEdad = parseInt(document.getElementById("txtIdEdad").value);
+
+	//aca hay que negar que es adolescente, decir que no es
+	//aca estoy negando agregando el negativo !
+
+	if (!(vEdad >=13 && vEdad <=17))
+	{
+	alert("No es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,28 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
+    //tomo de document faren y muestro por alert centi
+    let vTemperatura;
+    let vCentigrados;
+
+    vTemperatura = document.getElementById("txtIdTemperatura").value;
+
+    vCentigrados = (vTemperatura - 32) * 5 / 9;
+
+    alert(vTemperatura+"°F son "+vCentigrados.toFixed(2)+"°C");
 	
 }
 
 function CentigradosFahrenheit () 
 {
+    //paso la inversa de arriba
+    let vTemperatura;
+    let vFarenheit;
+
+    vTemperatura = document.getElementById("txtIdTemperatura").value;
+//cambia la operacion se suma 32 y se multi po 9 desp / 5
+    vFarenheit = (vTemperatura * 9 / 5) + 32;
+
+    alert(vTemperatura+"°C son "+vFarenheit.toFixed(2)+"°F");
 	
 }
